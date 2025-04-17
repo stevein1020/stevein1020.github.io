fb2ea9b88f7e4f82e74fed45496497492b7b60c6
Steven Chang <zminsc@seas.upenn.edu>
2025-04-17 15:24:30 -0400

    feat: clicking on chair displays chair image

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const baseImage = document.getElementById("bedroom-image");
 
 const beanbagImageSrc = "portfolio/assets/beanbag.jpeg";
+const chairImageSrc = "portfolio/assets/chair.jpeg"
 
 const bedroomOverlay = document.getElementById("bedroom-overlay");
 const beanbagOverlay = document.getElementById("beanbag-overlay");
+const chairOverlay = document.getElementById("chair-overlay")
 
 document.getElementById("beanbag-link").addEventListener("click", function (e) {
   e.preventDefault();
@@ -12,4 +14,13 @@ document.getElementById("beanbag-link").addEventListener("click", function (e) {
   baseImage.alt = "beanbag view";
   bedroomOverlay.classList.add("hidden");
   beanbagOverlay.classList.remove("hidden");
+});
+
+document.getElementById("chair-link").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  baseImage.src = chairImageSrc;
+  baseImage.alt = "chair view";
+  bedroomOverlay.classList.add("hidden");
+  chairOverlay.classList.remove("hidden");
 });
